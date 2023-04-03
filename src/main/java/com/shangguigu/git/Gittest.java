441a1cde72975325ad0112a682752fc2379a12ac
dengao <xuliyouxiag@.com>
2023-04-04 06:01:00 +0800

    first commit

---
+++ src/main/java/com/shangguigu/git/Gittest.java
@@ -0,0 +1,8 @@
+package com.shangguigu.git;
+
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("二次配置");
+    }
+}
---
+++ src/main/java/com/shangguigu/git/Gittesttwo.java
@@ -0,0 +1,7 @@
+package com.shangguigu.git;
+
+public class Gittesttwo {
+    public static void main(String[] args) {
+        System.out.println("fsdfsdfsd");
+    }
+}
