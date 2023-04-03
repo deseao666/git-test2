9e9bbbe03dc0d1b92b5309bf35b4b7671a30a733
dengao <xuliyouxiag@.com>
2023-04-04 06:17:38 +0800

    second commit

--- src/main/java/com/shangguigu/git/Gittest.java
+++ src/main/java/com/shangguigu/git/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("二次配置");
+        System.out.println("三次配置");
     }
 }
