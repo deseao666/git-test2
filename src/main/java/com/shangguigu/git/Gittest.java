25248beca4b8d0a3fc297eac99d5567c25f1fc21
deseao666 <129779558+deseao666@users.noreply.github.com>
2023-04-04 08:40:53 +0800

    pull-test

--- src/main/java/com/shangguigu/git/Gittest.java
+++ src/main/java/com/shangguigu/git/Gittest.java
@@ -9,6 +9,7 @@ public class Gittest {
         System.out.println("热更新");
         System.out.println("master-test");
         System.out.println("fox-test");
+        System.out.println("pull-test");
 
     }
 }
